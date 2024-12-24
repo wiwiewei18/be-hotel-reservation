bd2e2cef1a41eecfd23fa8e8557962a0a08f2b73
Wiwie Sanjaya <wiwiewei18@gmail.com>
2024-12-24 14:52:52 +0700

    feat: add e2e test

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -16,9 +16,9 @@ export class AuthController {
     @CurrentUser() user: UserDocument,
     @Res({ passthrough: true }) response: Response,
   ) {
-    await this.authService.login(user, response);
+    const jwt = await this.authService.login(user, response);
 
-    response.send(user);
+    response.send(jwt);
   }
 
   @UseGuards(JwtAuthGuard)
--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -25,5 +25,7 @@ export class AuthService {
     const token = this.jwtService.sign(tokenPayload);
 
     response.cookie('Authentication', token, { httpOnly: true, expires });
+
+    return token;
   }
 }
